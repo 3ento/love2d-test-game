7e793eeca4af6ad4d39a76eb108658a1f14496be
3ento <blborismichael@gmail.com>
2025-02-04 18:56:18 +0200

    organized dialogues.lua

--- main.lua
+++ main.lua
@@ -7,29 +7,16 @@ require 'scripts/dialogues'
 require 'scripts/mapRenderOrder'
 require 'scripts/runes'
 
-LoveDialogue = require "lib/Love-Dialogue-main/LoveDialogue"
 function love.load() 
-    --[[showRune1 = false
-    showRune2 = false
-    showRune3 = false
-    racTextBox = false]]
 
     setUpDependencies()
     racoonLoad()
     playerLoad()
     setUpColliders()
     loadRunes()
+    setUpDialogues()
 
-    myDialogue = LoveDialogue.play("rsc/dialogues/rac.ld")
-    runeMssg1 = LoveDialogue.play("rsc/dialogues/rune1.ld")
-    runeMssg2 = LoveDialogue.play("rsc/dialogues/rune2.ld")
-    runeMssg3 = LoveDialogue.play("rsc/dialogues/rune3.ld")
-    runeMssg4 = LoveDialogue.play("rsc/dialogues/rune4.ld")
-    runeMssg5 = LoveDialogue.play("rsc/dialogues/rune5.ld")
-    runeMssg6 = LoveDialogue.play("rsc/dialogues/rune6.ld")
-
-    allDialogue = {myDialogue, runeMssg1, runeMssg2, runeMssg3, runeMssg4, runeMssg5, runeMssg6}
-end
+    end
 
 function love.update(dt)
     interactPrompt.animation:update(dt)
@@ -38,14 +25,7 @@ function love.update(dt)
     camPreventOutOfBounds()
     collidersUpdate(dt)
     distanceDependentEvents()
-
-    myDialogue:update(dt)
-    runeMssg1:update(dt)
-    runeMssg2:update(dt)
-    runeMssg3:update(dt)
-    runeMssg4:update(dt)
-    runeMssg5:update(dt)
-    runeMssg6:update(dt)
+    updateDialogues(dt)
 end
 
 function love.draw()
@@ -57,89 +37,12 @@ function love.draw()
         f3MenuCam()
     cam:detach()
     f3MenuFixed()
-
-    if showRune1 then 
-        runeMssg1:draw()
-    end
-
-    if showRune2 then
-        runeMssg2:draw()
-    end
-
-    if racTextBox then
-        myDialogue:draw()
-    end
-
-    if showRune3 then 
-        runeMssg3:draw()
-    end
-
-    if showRune4 then 
-        runeMssg4:draw()
-    end
-
-    if showRune5 then 
-        runeMssg5:draw()
-    end
-    if showRune6 then 
-        runeMssg6:draw()
-    end
+    drawDialogues()
 end
 
 function love.keypressed(key)
     if key == "e" then
-        if calculateDistance(player, rac) < 180 then
-            if racTextBox then 
-                myDialogue.isActive = not myDialogue.isActive
-            end
-            racTextBox = true
-        end
-
-        for i, obj in pairs(runes) do
-            if calculateDistance(player, obj) < 80 then
-                if obj.idx == "1" then
-                    if showRune1 then
-                        runeMssg1.isActive = not runeMssg1.isActive
-                    end
-                    showRune1 = true
-                end
-
-                if obj.idx == "2" then
-                    if showRune2 then 
-                        runeMssg2.isActive = not runeMssg2.isActive
-                    end
-                    showRune2 = true
-                end
-
-                if obj.idx == "3" then
-                    if showRune3 then 
-                        runeMssg3.isActive = not runeMssg3.isActive
-                    end
-                    showRune3 = true
-                end
-
-                if obj.idx == "4" then
-                    if showRune4 then 
-                        runeMssg4.isActive = not runeMssg4.isActive
-                    end
-                    showRune4 = true
-                end
-
-                if obj.idx == "5" then
-                    if showRune5 then 
-                        runeMssg5.isActive = not runeMssg5.isActive
-                    end
-                    showRune5 = true
-                end
-
-                if obj.idx == "6" then
-                    if showRune6 then 
-                        runeMssg6.isActive = not runeMssg6.isActive
-                    end
-                    showRune6 = true
-                end
-            end
-        end
+        interactionModules()
     end
 
     -- advance text boxes
--- scripts/dialogues.lua
+++ scripts/dialogues.lua
@@ -1,19 +1,103 @@
-function advanceDialogue(condition, dialogueObject)
-    if condition then 
-        dialogueObject.isActive = not dialogueObject.isActive
-        dialogueObject.currentLine = 1
-        return true
-    end
-    return true
+LoveDialogue = require "lib/Love-Dialogue-main/LoveDialogue"
+
+function setUpDialogues() 
+    myDialogue = LoveDialogue.play("rsc/dialogues/rac.ld")
+    runeMssg1 = LoveDialogue.play("rsc/dialogues/rune1.ld")
+    runeMssg2 = LoveDialogue.play("rsc/dialogues/rune2.ld")
+    runeMssg3 = LoveDialogue.play("rsc/dialogues/rune3.ld")
+    runeMssg4 = LoveDialogue.play("rsc/dialogues/rune4.ld")
+    runeMssg5 = LoveDialogue.play("rsc/dialogues/rune5.ld")
+    runeMssg6 = LoveDialogue.play("rsc/dialogues/rune6.ld")
+
+    allDialogue = {myDialogue, runeMssg1, runeMssg2, runeMssg3, runeMssg4, runeMssg5, runeMssg6}
 end
 
-
---[[if calculateDistance(player, obj) < 200 then
-    if obj.idx == "1" then
-        if test1 then 
-            mySecondDialogue.isActive = not mySecondDialogue.isActive
-            mySecondDialogue.currentLine = 1
-        end
-        test1 = true
+function updateDialogues(dt) 
+    for i, obj in pairs(allDialogue) do 
+        obj:update(dt)
     end
-end]]
+end
+
+function drawDialogues() 
+    if showRune1 then 
+        runeMssg1:draw()
+    end
+
+    if showRune2 then
+        runeMssg2:draw()
+    end
+
+    if racTextBox then
+        myDialogue:draw()
+    end
+
+    if showRune3 then 
+        runeMssg3:draw()
+    end
+
+    if showRune4 then 
+        runeMssg4:draw()
+    end
+
+    if showRune5 then 
+        runeMssg5:draw()
+    end
+    if showRune6 then 
+        runeMssg6:draw()
+    end
+end
+
+function interactionModules() 
+    if calculateDistance(player, rac) < 180 then
+        if racTextBox then 
+            myDialogue.isActive = not myDialogue.isActive
+        end
+        racTextBox = true
+    end
+
+    for i, obj in pairs(runes) do
+        if calculateDistance(player, obj) < 80 then
+            if obj.idx == "1" then
+                if showRune1 then
+                    runeMssg1.isActive = not runeMssg1.isActive
+                end
+                showRune1 = true
+            end
+
+            if obj.idx == "2" then
+                if showRune2 then 
+                    runeMssg2.isActive = not runeMssg2.isActive
+                end
+                showRune2 = true
+            end
+
+            if obj.idx == "3" then
+                if showRune3 then 
+                    runeMssg3.isActive = not runeMssg3.isActive
+                end
+                showRune3 = true
+            end
+
+            if obj.idx == "4" then
+                if showRune4 then 
+                    runeMssg4.isActive = not runeMssg4.isActive
+                end
+                showRune4 = true
+            end
+
+            if obj.idx == "5" then
+                if showRune5 then 
+                    runeMssg5.isActive = not runeMssg5.isActive
+                end
+                showRune5 = true
+            end
+
+            if obj.idx == "6" then
+                if showRune6 then 
+                    runeMssg6.isActive = not runeMssg6.isActive
+                end
+                showRune6 = true
+            end
+        end
+    end
+end
